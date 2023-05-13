196e51b6c90311a6dea09110e0c1906c217d48e8
Tozy Fullbuster <tozyfullbuster@gmail.com>
2023-05-13 19:47:18 +0700

    feat: add Version model

---
+++ src/main/kotlin/io/github/tozyf/versions/Version.kt
@@ -0,0 +1,31 @@
+package io.github.tozyf.versions
+
+/**
+ * Represents a parsed version.
+ *
+ * The implementation of this interface must be immutable.
+ */
+public interface Version : Comparable<Version> {
+    /**
+     * Returns the string representation of this version.
+     */
+    public override fun toString(): String
+}
+
+/**
+ * Creates an instance of [Version] from the given [versionString]. The created instance is immutable and has no
+ * special meaning (has no scheme applied).
+ *
+ * The comparison of two versions is based on the lexicographic order of their string representations.
+ */
+public fun Version(versionString: String): Version = ImmutableVersion(versionString)
+
+private class ImmutableVersion(private val versionString: String) : Version {
+    override fun toString(): String = versionString
+
+    override fun compareTo(other: Version): Int = versionString.compareTo(other.toString())
+
+    override fun equals(other: Any?): Boolean = other is Version && versionString == other.toString()
+
+    override fun hashCode(): Int = versionString.hashCode()
+}
