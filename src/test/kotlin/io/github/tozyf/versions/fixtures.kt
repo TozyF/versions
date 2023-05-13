a6bc4c0bf7d2c11a2edc90882d6cb296d25b465a
Tozy Fullbuster <tozyfullbuster@gmail.com>
2023-05-13 20:26:56 +0700

    test: add Version model test

---
+++ src/test/kotlin/io/github/tozyf/versions/fixtures.kt
@@ -0,0 +1,17 @@
+package io.github.tozyf.versions
+
+@Suppress("ObjectPropertyName")
+object Versions {
+    val `0_0_0` = Version("0.0.0")
+    val `1_0_0` = Version("1.0.0")
+
+    val `1_0_0-snapshot` = Version("1.0.0-snapshot")
+    val `1_0_0-SNAPSHOT` = Version("1.0.0-SNAPSHOT")
+
+    val unspecified = Version("unspecified")
+    val UNSPECIFIED = Version("UNSPECIFIED")
+
+    val abc = Version("abc")
+    val bce = Version("bce")
+    val def = Version("def")
+}
