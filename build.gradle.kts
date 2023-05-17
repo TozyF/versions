0e496c34931d8bc679f497a7e3f033eaf4e46bfd
Tozy Fullbuster <114842493+TozyF@users.noreply.github.com>
2023-05-17 19:46:31 +0700

    build: configure publication (#2)
    * build: add publication
    
    * build: fix sources task

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,11 @@ fun properties(key: String): Provider<String> = providers.gradleProperty(key)
 
 plugins {
     `java-library`
+    `maven-publish`
+    signing
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlinx.kover)
+    alias(libs.plugins.dokka)
 }
 
 version = properties("version").get()
@@ -17,7 +20,7 @@ description = properties("description").get()
 
 testing {
     suites {
-        val test by getting(JvmTestSuite::class) {
+        named<JvmTestSuite>("test") {
             useKotlinTest(libs.versions.kotlin)
         }
     }
@@ -32,6 +35,62 @@ tasks {
     withType<KotlinCompile> {
         kotlinOptions {
             jvmTarget = JvmTarget.JVM_17.target
+        }
+    }
+
+    register<Jar>("sourcesJar") {
+        from(sourceSets.main.map { it.allSource })
+        archiveClassifier = "sources"
+    }
+
+    register<Jar>("dokkaJavadocJar") {
+        dependsOn(dokkaJavadoc)
+        from(dokkaJavadoc.flatMap { it.outputDirectory })
+        archiveClassifier = "javadoc"
+    }
+}
+
+signing {
+    sign(publishing.publications)
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = properties("sonatype.user").get()
+                username = properties("sonatype.password").get()
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["dokkaJavadocJar"])
+            pom {
+                description = project.description
+                url = properties("url")
+                licenses {
+                    license {
+                        name = "Apache License 2.0"
+                        url = "https://github.com/TozyF/versions/blob/main/LICENSE"
+                        distribution = "repo"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "tozyf"
+                        name = "Tozy Fullbuster"
+                        url = "https://github.com/TozyF"
+                    }
+                }
+                scm {
+                    url = properties("url")
+                }
+            }
         }
     }
 }
